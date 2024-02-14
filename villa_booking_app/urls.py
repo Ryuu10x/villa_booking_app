c2dcc60c74a1ccda8c8bda27adc64ef1e10f2ee1
waihpone <whharry7@gmail.com>
2024-02-15 01:53:57 +0800

    Contact Us Page done

--- villa_booking_app/urls.py
+++ villa_booking_app/urls.py
@@ -4,13 +4,12 @@ from django.urls import path
 from . import views
 from .views import CustomLoginView
 
-app_name = 'villa_booking_app'
+app_name = "villa_booking_app"
 urlpatterns = [
-    #Home page
-    path('', views.index, name="index"),
-    path('about/', views.about, name="about"),
-    path('villa/<int:villa_id>/', views.villa_detail, name='villa_info'),
-    path('villa/<int:villa_id>/book/', views.book_villa, name='book_villa'),
-
+    # Home page
+    path("", views.index, name="index"),
+    path("about/", views.about, name="about"),
+    path("contact/", views.contact, name="contact"),
+    path("villa/<int:villa_id>/", views.villa_detail, name="villa_info"),
+    path("villa/<int:villa_id>/book/", views.book_villa, name="book_villa"),
 ]
-
--- villa_booking_app/views.py
+++ villa_booking_app/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from django.db.models import Q
 from .models import Villa, Booking
-from .forms import BookingForm #Create a form in the next step
+from .forms import BookingForm  # Create a form in the next step
 from datetime import timedelta
 
 # Create your views here.
@@ -9,45 +9,58 @@ from datetime import timedelta
 # views.py
 from django.contrib.auth.views import LoginView
 
+
 class CustomLoginView(LoginView):
-    template_name = 'login.html'  # Your login template
+    template_name = "login.html"  # Your login template
     redirect_authenticated_user = True
+
 
 def index(request):
     # The home page for villa_booking_app
     villas = Villa.objects.all()
-    context = {'villas': villas}
-    return render(request, 'villa_booking_app/home.html', context)
+    context = {"villas": villas}
+    return render(request, "villa_booking_app/home.html", context)
+
 
 def about(request):
     return render(request, "villa_booking_app/about.html")
 
-#display villa info
-def villa_detail(request, villa_id):
-    villa = get_object_or_404(Villa, pk = villa_id)
 
-    #get booked dates for the villa
-    booked_dates = Booking.objects.filter(villa=villa).values_list('check_in_date', 'check_out_date')
+def contact(request):
+    return render(request, "villa_booking_app/contact.html")
+
+
+# display villa info
+def villa_detail(request, villa_id):
+    villa = get_object_or_404(Villa, pk=villa_id)
+
+    # get booked dates for the villa
+    booked_dates = Booking.objects.filter(villa=villa).values_list(
+        "check_in_date", "check_out_date"
+    )
 
     context = {
-        'villa':villa,
-        'booked_dates': booked_dates,
+        "villa": villa,
+        "booked_dates": booked_dates,
     }
-    return render(request, 'villa_booking_app/villa_info.html', context)
+    return render(request, "villa_booking_app/villa_info.html", context)
 
-#view to handle booking data
-#function to handle date overlaps and calculations (probabaly better refactored)
+
+# view to handle booking data
+# function to handle date overlaps and calculations (probabaly better refactored)
 def book_villa(request, villa_id):
     villa = get_object_or_404(Villa, pk=villa_id)
 
-    if request.method == 'POST':
+    if request.method == "POST":
         form = BookingForm(request.POST)
         if form.is_valid():
             booking = form.save(commit=False)
 
-            #get booked dates for this villa
-            booked_dates = Booking.objects.filter(villa=villa).values_list('check_in_date', 'check_out_date')
-            #check availablity (overlaps)
+            # get booked dates for this villa
+            booked_dates = Booking.objects.filter(villa=villa).values_list(
+                "check_in_date", "check_out_date"
+            )
+            # check availablity (overlaps)
             # if Booking.objects.filter(
             # (Q(check_in_date__range=[booking.check_in_date, booking.check_out_date]) |
             # Q(check_out_date__range=[booking.check_in_date, booking.check_out_date])),
@@ -62,26 +75,46 @@ def book_villa(request, villa_id):
             #     return render(request, 'villa_booking_app/booking_error.html', {'error_message': 'Selected dates are not available.', 'villa': villa, 'booked_dates': booked_dates})
             if Booking.objects.filter(
                 (
-                    Q(check_in_date__range=[booking.check_in_date, booking.check_out_date]) |
-                    Q(check_out_date__range=[booking.check_in_date, booking.check_out_date]) |
-                    (Q(check_in_date__lte=booking.check_in_date) & Q(check_out_date__gte=booking.check_out_date))
+                    Q(
+                        check_in_date__range=[
+                            booking.check_in_date,
+                            booking.check_out_date,
+                        ]
+                    )
+                    | Q(
+                        check_out_date__range=[
+                            booking.check_in_date,
+                            booking.check_out_date,
+                        ]
+                    )
+                    | (
+                        Q(check_in_date__lte=booking.check_in_date)
+                        & Q(check_out_date__gte=booking.check_out_date)
+                    )
                 ),
                 villa=villa,
             ).exists():
-                return render(request, 'villa_booking_app/booking_error.html', {'error_message': 'Selected dates are not available.', 'villa': villa, 'booked_dates': booked_dates})
-
-
+                return render(
+                    request,
+                    "villa_booking_app/booking_error.html",
+                    {
+                        "error_message": "Selected dates are not available.",
+                        "villa": villa,
+                        "booked_dates": booked_dates,
+                    },
+                )
 
             booking.villa = villa
             booking.user = request.user
 
-            #cost calculation
-            booking.total_cost = (booking.check_out_date - booking.check_in_date).days * villa.price_per_night
+            # cost calculation
+            booking.total_cost = (
+                booking.check_out_date - booking.check_in_date
+            ).days * villa.price_per_night
             booking.save()
-            return redirect('villa_booking_app:villa_info', villa_id=villa_id)
+            return redirect("villa_booking_app:villa_info", villa_id=villa_id)
     else:
         form = BookingForm()
-    
 
-    context = {'form': form, 'villa': villa}
-    return render(request, 'villa_booking_app/booking_form.html', context)
+    context = {"form": form, "villa": villa}
+    return render(request, "villa_booking_app/booking_form.html", context)
